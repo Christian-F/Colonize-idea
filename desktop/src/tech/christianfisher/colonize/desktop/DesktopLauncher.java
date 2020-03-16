05ea95455ecd5c3755c28e387696bae459d4c860
Christian-F <christian@christianfisher.tech>
2020-03-15 21:55:58 -0600

    Initial commit

---
+++ desktop/src/tech/christianfisher/colonize/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tech.christianfisher.colonize.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tech.christianfisher.colonize.ColonizeGameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ColonizeGameClass(), config);
+	}
+}
