07ae85e82ee55c2954f664dd10e67491f9d0914b
Christian-F <christian@christianfisher.tech>
2020-03-17 23:14:08 -0600

    Added Screen skeleton classes. Started changeScreen

--- core/src/tech/christianfisher/colonize/ColonizeGameClass.java
+++ core/src/tech/christianfisher/colonize/ColonizeGameClass.java
@@ -1,12 +1,23 @@
 package tech.christianfisher.colonize;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class ColonizeGameClass extends ApplicationAdapter {
+
+/**
+ * This is an Oxygen Not Included style 2d, colony building/management game.
+ *
+ * This is the tutorial I'm following, so I don't loose yet:
+ * https://www.gamedevelopment.blog/full-libgdx-game-tutorial-project-setup/
+ *
+ * @author Christian Fisher
+ */
+
+public class ColonizeGameClass extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
@@ -29,5 +40,16 @@ public class ColonizeGameClass extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+	}
+
+	public void changeScreen(int screen) {
+		switch(screen) {
+			case MENU:
+				if(menuScreen == null) menuScreen  new MenuScreen();
+					this.setScreen(menuScreen);
+				break;
+			case PREFERENCES:
+
+		}
 	}
 }
