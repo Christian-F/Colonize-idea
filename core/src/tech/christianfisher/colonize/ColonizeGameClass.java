1ab554ce08a44939d459c46f9c313813f38dd51f
christian.fisher.7@gmail.com <christian@christianfisher.tech>
2020-03-21 16:04:22 -0600

    Added preferences menu. Fixed screen switching.

--- core/src/tech/christianfisher/colonize/ColonizeGameClass.java
+++ core/src/tech/christianfisher/colonize/ColonizeGameClass.java
@@ -1,6 +1,5 @@
 package tech.christianfisher.colonize;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -12,7 +11,7 @@ import tech.christianfisher.colonize.screens.*;
 /**
  * This is an Oxygen Not Included style 2d, colony building/management game.
  *
- * This is the tutorial I'm following, so I don't loose yet:
+ * This is the tutorial I'm following, so I don't loose it:
  * https://www.gamedevelopment.blog/full-libgdx-game-tutorial-project-setup/
  *
  * @author Christian Fisher
@@ -28,26 +27,35 @@ public class ColonizeGameClass extends Game {
 	private MainScreen mainScreen;
 	private EndScreen endScreen;
 
+	private AppPreferences preferences;
+
 	public final static int MENU = 0;
 	public final static int PREFERENCES = 1;
 	public final static int APPLICATION = 2;
 	public final static int ENDGAME = 3;
 
+	public AppPreferences getPreferences(){
+		return this.preferences;
+	}
+
 	@Override
 	public void create () {
+		preferences = new AppPreferences();
 		loadingScreen = new LoadingScreen(this);
-		setScreen(loadingScreen);
+		this.setScreen(loadingScreen);
+
 	}
 
 	@Override
 	public void render () {
+		Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		super.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 
 	public void changeScreen(int screen) {
@@ -58,15 +66,15 @@ public class ColonizeGameClass extends Game {
 				break;
 			case PREFERENCES:
 				if(preferencesScreen == null) preferencesScreen = new PreferencesScreen(this);
-				this.setScreen(menuScreen);
+					this.setScreen(menuScreen);
 				break;
 			case APPLICATION:
 				if (mainScreen == null) mainScreen = new MainScreen(this);
-				this.setScreen(mainScreen);
+					this.setScreen(mainScreen);
 				break;
 			case ENDGAME:
 				if (endScreen == null) endScreen = new EndScreen(this);
-				this.setScreen(endScreen);
+					this.setScreen(endScreen);
 				break;
 		}
 	}
--- core/src/tech/christianfisher/colonize/screens/PreferencesScreen.java
+++ core/src/tech/christianfisher/colonize/screens/PreferencesScreen.java
@@ -2,26 +2,129 @@ package tech.christianfisher.colonize.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import tech.christianfisher.colonize.ColonizeGameClass;
 
 public class PreferencesScreen implements Screen {
 
     private ColonizeGameClass parent;
+    private Stage stage;
+
+    //skin path
+    private String skinPath;
+
+    //labels for buttons/sliders
+    private Label titleLabel;
+    private Label volumeMusicLabel;
+    private Label volumeSoundLabel;
+    private Label musicOnOffLabel;
+    private Label soundOnOffLabel;
 
     public PreferencesScreen(ColonizeGameClass game){
         parent = game;
+        skinPath = "glassy/skin/glassy-ui.json";
+        stage = new Stage(new ScreenViewport());
 
+        Table table = new Table();
+
+
+        Skin skin = new Skin(Gdx.files.internal(skinPath));
+
+        //music volume
+        final Slider volumeMusicSlider = new Slider(0f, 1f, 0.1f, false, skin);
+        volumeMusicSlider.setValue(parent.getPreferences().getMusicVolume());
+        volumeMusicSlider.addListener(new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                parent.getPreferences().setPrefMusicVolume(volumeMusicSlider.getValue());
+                return false;
+            }
+        });
+        //sound effects volume
+        final Slider volumeSoundSlider = new Slider(0f, 1f, 0.1f, false, skin);
+        volumeSoundSlider.setValue(parent.getPreferences().getSoundEffectsVolume());
+        volumeSoundSlider.addListener(new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                parent.getPreferences().setSoundEffectsVolume(volumeSoundSlider.getValue());
+                return false;
+            }
+        });
+        //music enabled
+        final CheckBox musicCheckBox = new CheckBox(null, skin);
+        musicCheckBox.setChecked(parent.getPreferences().isMusicEnable());
+        musicCheckBox.addListener(new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                boolean enabled = musicCheckBox.isChecked();
+                parent.getPreferences().setMusicEnable(enabled);
+                return false;
+            }
+        });
+        //sound effects enabled
+        final CheckBox soundCheckBox = new CheckBox(null, skin);
+        soundCheckBox.setChecked(parent.getPreferences().isSoundsEffectsEnabled());
+        soundCheckBox.addListener(new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                boolean enabled = soundCheckBox.isChecked();
+                parent.getPreferences().setSoundEffectsEnabled(enabled);
+                return false;
+            }
+        });
+
+        //return to main menu
+        final TextButton backButton = new TextButton("Back", skin, "small");
+        backButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                parent.setScreen(new MenuScreen(parent));
+            }
+        });
+
+        titleLabel = new Label("Preferences", skin);
+        volumeMusicLabel = new Label("Music Volume", skin);
+        volumeSoundLabel = new Label("Sound Volume", skin);
+        musicOnOffLabel = new Label("Music", skin);
+        soundOnOffLabel = new Label("Sound", skin);
+
+        table.add(titleLabel).colspan(2);
+        table.row().pad(10,0,0,10);
+        table.add(volumeMusicLabel);
+        table.add(volumeMusicSlider);
+        table.row().pad(10,0,0,10);
+        table.add(musicOnOffLabel);
+        table.add(musicCheckBox);
+        table.row().pad(10,0,0,10);
+        table.add(volumeSoundLabel);
+        table.add(volumeSoundSlider);
+        table.row().pad(10,0,0,10);
+        table.add(soundOnOffLabel);
+        table.add(soundCheckBox);
+        table.row().pad(10,0,0,10);
+        table.add(backButton).pad(10,0,0,10);
+
+        table.setFillParent(true);
+        table.setDebug(false);
+        this.stage.addActor(table);
     }
 
     @Override
     public void show() {
+        Gdx.input.setInputProcessor(this.stage);
 
     }
 
     @Override
     public void render(float delta) {
-
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1/30f));
+        stage.draw();
     }
 
     @Override
@@ -46,6 +149,6 @@ public class PreferencesScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        stage.dispose();
     }
 }
